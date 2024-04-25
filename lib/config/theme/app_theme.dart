4fc80daca63ec51c4d099d3a6d3c27f8739fdf9d
Palejandrachavez <a19490311@itmexicali.edu.mx>
2024-04-25 00:08:17 -0700

    cambio color

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   ThemeData getTheme() {
-    const seedColor = Colors.deepPurple;
+    const seedColor = Color.fromARGB(255, 110, 172, 131);
 
     return ThemeData(
         useMaterial3: true,
