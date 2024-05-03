3f693c6f82376826075a5d33f4ea777ba1745cf4
Palejandrachavez <a19490311@itmexicali.edu.mx>
2024-05-02 20:55:00 -0700

    modificacion bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:practicapushnotification/config/router/app_router.dart';
 import 'package:practicapushnotification/config/theme/app_theme.dart';
+import 'package:practicapushnotification/firebase_options.dart';
 import 'package:practicapushnotification/presentation/notifications/bloc/notifications_bloc.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
--- lib/presentation/notifications/bloc/notifications_bloc.dart
+++ lib/presentation/notifications/bloc/notifications_bloc.dart
@@ -1,10 +1,6 @@
-import 'dart:html';
-
 import 'package:equatable/equatable.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:practicapushnotification/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
@@ -16,12 +12,6 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     // on<NotificationsEvent>((event, emit) {
     //   // TODO: implement event handler
     // });
-  }
-
-  static Future<void> initializeFCM() async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
   }
 
   void requestPermission() async {
